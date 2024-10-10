6bdd8610f4b77401019c5ea0fe2ed59685f5dff4
sptungG <ngoviettung154@gmail.com>
2024-10-11 00:26:30 +0700

    update code

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  swcMinify: true,
+  compress: process.env.NODE_ENV === "production",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            // https://react-svgr.com/docs/options/
+            icon: true,
+            dimensions: false,
+            svgo: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+  headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          ...createSecureHeaders(),
+          // HSTS Preload: https://hstspreload.org/
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
