9a221980a2851c8a012e52cfe9e95895fdc3c3a5
sptungG <ngoviettung154@gmail.com>
2024-10-16 08:48:09 +0700

    update code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+import SEO from "@/components/next/SEO";
 import Fonts from "@/styles/fonts";
 
 export default function Home() {
-  return <div className={`${Fonts.fontGeistSans.variable} ${Fonts.fontGeistMono.variable}`}></div>;
+  return (
+    <div className="">
+      <SEO />
+    </div>
+  );
 }
