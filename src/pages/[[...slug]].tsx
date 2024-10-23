c582a88e9bfc5c7bcdfaf8b2b71e65238cfa3b8b
sptungG <ngoviettung154@gmail.com>
2024-10-23 15:54:07 +0700

    update title seo

--- src/pages/[[...slug]].tsx
+++ src/pages/[[...slug]].tsx
@@ -50,10 +50,7 @@ const Page = (props: { data: Sheet }) => {
 
   return (
     <>
-      <SEO
-        title={[userData?.fullName, "Welcome to Our Wedding", "Cuong&Linh"].filter(Boolean).join(" | ")}
-        description={Array(11).fill("🎉").join("")}
-      />
+      <SEO title={["Welcome to Our Wedding", "Cuong&Linh"].join(" | ")} description={["🎊", userData?.fullName, "🎉"].filter(Boolean).join(" | ")} />
 
       <section className="relative flex min-h-dvh max-w-[100dvw] flex-col items-center justify-center overflow-hidden max-sm:py-20 sm:max-h-[1000px]">
         <div className="z-10 flex items-center max-sm:flex-col sm:justify-between">
