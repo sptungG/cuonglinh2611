5f8f421955980e852f4cfeae9a8dc6da07e16ab0
sptungG <ngoviettung154@gmail.com>
2024-10-24 10:11:59 +0700

    update toast

--- src/components/animation/Provider.tsx
+++ src/components/animation/Provider.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { LazyMotion, domAnimation } from "framer-motion";
 import { Toaster } from "../toast/Toaster";
+import { useMediaQuery } from "react-responsive";
 
 type TProviderProps = { children?: React.ReactNode };
 
 const Provider = ({ children }: TProviderProps) => {
+  const mediaAbove640 = useMediaQuery({ minWidth: 640 });
   return (
     <LazyMotion features={domAnimation}>
       {children}
-      <Toaster />
+      <Toaster position={mediaAbove640 ? "bottom-right" : "top-right"} />
     </LazyMotion>
   );
 };
