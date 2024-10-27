2a1317db7ec204d8a132f988c064fe0b6eb59828
sptungG <ngoviettung154@gmail.com>
2024-10-27 22:35:27 +0700

    update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Page = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="flex h-dvh flex-col items-center justify-center bg-white dark:bg-gray-900">
       <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
         <div className="mx-auto max-w-screen-sm text-center">
           <h1 className="mb-4 text-7xl font-extrabold tracking-tight text-amber-600 dark:text-amber-500 lg:text-9xl">404</h1>
@@ -13,7 +13,7 @@ const Page = () => {
           </p>
           <Link
             href="/c"
-            className="my-4 inline-flex rounded-lg bg-amber-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-amber-800 focus:outline-none focus:ring-4 focus:ring-amber-300 dark:focus:ring-amber-900"
+            className="my-4 inline-flex rounded-lg bg-amber-600 px-5 py-2.5 text-center text-base font-[600] text-white hover:bg-amber-800 focus:outline-none focus:ring-4 focus:ring-amber-300 dark:focus:ring-amber-900"
           >
             Back to Homepage
           </Link>
