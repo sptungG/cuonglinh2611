2043bb6864f22fc5c5d4d9d91ee59751238df317
sptungG <ngoviettung154@gmail.com>
2024-10-27 17:51:16 +0700

    update album

--- src/components/sections/SectionAlbum.tsx
+++ src/components/sections/SectionAlbum.tsx
@@ -59,7 +59,7 @@ const SectionAlbum = () => {
   const virtualizer = useWindowVirtualizer({
     count: ALBUMS.length,
     estimateSize: (i) => 100,
-    overscan: mediaAbove640 ? 5 : 2,
+    overscan: mediaAbove640 ? 5 : 20,
     lanes: mediaAbove640 ? 5 : 2,
     gap: mediaAbove640 ? 20 : 10,
     scrollMargin: 0,
