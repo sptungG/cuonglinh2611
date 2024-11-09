02a9c81dd59b260ed21772df05b650ec518bb62a
sptungG <ngoviettung154@gmail.com>
2024-11-10 02:04:53 +0700

    update  music

--- src/components/sections/SectionAlbum.tsx
+++ src/components/sections/SectionAlbum.tsx
@@ -11,6 +11,7 @@ import PreviewImagesProvider from "../context/PreviewImagesContext";
 import { IMG_BLUR } from "@/common/constant";
 // shuffleArray
 // shuffleArray
+// shuffleArray
 function shuffleArray(array: string[]) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
